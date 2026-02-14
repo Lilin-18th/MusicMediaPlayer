47889849e78ccca19e7c3316a847fa9d14b7de1e
lilin <st.yuto0410@gmail.com>
2026-02-14 17:04:17 +0900

    ColorとTheme整理

--- app/src/main/java/com/lilin/musicmediaplayer/ui/theme/Color.kt
+++ app/src/main/java/com/lilin/musicmediaplayer/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.lilin.musicmediaplayer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PlayerBackgroundTop = Color(0xFF2A1545)
+val PlayerBackgroundMiddle = Color(0xFF12081F)
+val PlayerBackgroundBottom = Color(0xFF0A0612)
+val PlayerGlowPurple = Color(0xFFA855F7)
+val PlayerGlowBlue = Color(0xFF3B82F6)
+val PlayerAccent = Color(0xFF818CF8)
+val PlayerSurfaceVariant = Color(0xFF1A1028)
